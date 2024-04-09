ebb08dc43a161236a5b94ddd436712645075c310
Stephen Muñez <sjtmunez@gmail.com>
2024-04-09 14:05:52 +0800

    setup: fonts and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const mouseMemoirs = localFont({
+  src: "../public/fonts/MouseMemoirs-Regular.ttf",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={mouseMemoirs.className}>{children}</body>
     </html>
   );
 }
